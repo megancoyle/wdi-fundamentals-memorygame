eed33db421d1922afb4351069b8e0b02015241c8
Megan Coyle <meg@mcoyle.com>
2016-06-21 22:14:50 -0400

    Add for loop and create board function

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,12 +3,26 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
-  alert("You found a match!");
-} else if (cardOne === cardTwo) {
-  alert("You found a match!");
-} else if (cardThree === cardFour) {
-  alert("You found a match!");
-} else {
-  alert("Sorry, try again.");
-};
+// if (cardTwo === cardFour) {
+//   alert("You found a match!");
+// } else if (cardOne === cardTwo) {
+//   alert("You found a match!");
+// } else if (cardThree === cardFour) {
+//   alert("You found a match!");
+// } else {
+//   alert("Sorry, try again.");
+// };
+
+
+function createBoard() {
+// iterates through for loop 4 times (maybe find a cleaner way to do this)
+  for (i = 0; i < 4; i++) {
+    var gameBoard = document.getElementById('game-board');
+    var newCard = document.createElement('div');
+    newCard.className = 'card';
+    gameBoard.appendChild(newCard);
+  }
+}
+
+// triggers function to create the card board
+createBoard();
